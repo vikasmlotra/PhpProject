bfec6ce970526768de79de6dc250fba3f3e4228b
saisoft <saisoft@php-linux>
2015-02-25 00:36:51 -0500

    Hello World Code

--- helloworld.php
+++ helloworld.php
@@ -5,4 +5,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//This is a concatenated Hello World code.
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+<?php
+$string = "Hello";
+$string2 = "World";
+$string3 = "$string $string2";
+
+echo $string3;
+?>
+        
+    </body>
+</html>
+
 
